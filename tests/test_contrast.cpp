a85b79cff93a5377e9e7659f617ee743e1421282
Jonathan Tynan <jojaty@gmail.com>
2025-06-06 10:16:04 +1000

    chore: add comments to contrast test

--- tests/test_contrast.cpp
+++ tests/test_contrast.cpp
@@ -3,6 +3,13 @@
 #include "urColo/Colour.h"
 #include "urColo/Contrast.h"
 
+/**
+ * @brief Verify the maximum contrast ratio between black and white.
+ *
+ * This doctest ensures that a black foreground on a white background
+ * produces the expected 21:1 ratio and meets WCAG AAA requirements.
+ */
+
 TEST_CASE("black and white contrast") {
     uc::Colour black = uc::Colour::fromSRGB(0, 0, 0);
     uc::Colour white = uc::Colour::fromSRGB(255, 255, 255);
